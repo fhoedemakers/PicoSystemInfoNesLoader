8efe488df75124b5007010f72c82979cee22c367
Frank Hoedemakers <fhoedemakers@hotmail.com>
2023-07-28 09:41:38 +0200

    Version setting change.

---
+++ PicoNesLoader/PicoNesLoader/VersionInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicoSystemInfoNesLoader
+{
+    public static class VersionInfo
+    {
+        public static string CurrentVersion = "DEVVERSION";
+    }
+}
